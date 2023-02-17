e78dc3cf83b09450560a013cad81836385870ab4
xlang <xlangk@proton.me>
2023-02-17 12:58:31 -0500

    Feat: Added missing files

---
+++ src/renderer/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+    import TitleBar from "./components/TitleBar.svelte";
+    import Emulator from "./components/Emulator.svelte";
+    import SideBar from "./components/SideBar.svelte";
+</script>
+
+<TitleBar />
+<div class="content">
+	<SideBar />
+	<Emulator />
+</div>
+
+<style lang="scss">
+@import "Config.scss";
+
+.content {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+}
+</style>
