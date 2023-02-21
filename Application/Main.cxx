32c70b1737c8dc2fadf5a8c08f9db41f20c3b19d
Rayyan Khan <117920879+Xlangk@users.noreply.github.com>
2023-02-21 14:02:21 +0000

    [#5/#6] Implemented binary sub/add utils

--- Application/Main.cxx
+++ Application/Main.cxx
@@ -1,5 +1,31 @@
+#include <FireStone/DataTraffic>
 #include <iostream>
 
+/**
+ * @brief This will check to make sure that the packet translator can increment numbers.
+ * @return Whether an error exists.
+ */
 int main() {
-    return 0;
+    using namespace FireStone::DataTraffic;
+
+    PacketTranslator<4> translator;
+    uint16_t realNumber = 0;
+    bool packets[16][4] = {};
+    bool *lastPacket;
+
+    const bool truePacket[4] = { true, true, true, true };
+
+    for (size_t i = 0; i < 16; i++) {
+        if (i == 0) lastPacket = translator.Decrement(const_cast<bool *>(truePacket));
+        else lastPacket = translator.Decrement(lastPacket);
+        for (size_t j = 0; j < 4; j++)
+            packets[i][j] = lastPacket[j];
+    }
+
+    // print out all values
+    for (size_t i = 0; i < 16; i++) {
+        for (size_t j = 0; j < 4; j++)
+            std::cout << packets[i][j];
+        std::cout << std::endl;
+    }
 }
--- DataTraffic/Binary.hxx
+++ DataTraffic/Binary.hxx
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 namespace FireStone::DataTraffic {
     /**
      * @brief Class used for manipulating binary packets without converting them to the host integer. This
@@ -15,18 +17,88 @@ namespace FireStone::DataTraffic {
         static constexpr size_t Width = PacketWidth;
 
         /**
+         * @brief Add 2 binary packets together.
+         * @param packet1 The first packet.
+         * @param packet2 The second packet.
+         * @return The sum of the 2 packets.
+         */
+        bool *Add(bool packet1[PacketWidth], bool packet2[PacketWidth]) {
+            uint8_t cary = 0; // 0 (00) 1 (01) 2 (11)
+
+            for (int i = PacketWidth - 1; i >= 0; i--) {
+                uint8_t sum = packet1[i] + packet2[i] + cary;
+                if (sum == 0) {
+                    packet1[i] = false;
+                    cary = 0;
+                } else if (sum == 1) {
+                    packet1[i] = true;
+                    cary = 0;
+                } else if (sum == 2) {
+                    packet1[i] = false;
+                    cary = 1;
+                } else if (sum == 3) {
+                    packet1[i] = true;
+                    cary = 1;
+                }
+            }
+
+            return packet1;
+        }
+
+        /**
          * @brief Increment the value of the packet by 1.
          * @param packet The packet to increment.
          * @return The incremented packet.
          */
         bool *Increment(bool packet[PacketWidth]) {
-            for (size_t i = 0; i < PacketWidth; i++)
-                if (packet[i] == false) {
-                    packet[i] = true;
-                    break;
-                } else
-                    packet[i] = false;
-            return packet;
+            bool one[PacketWidth];
+            for (int i = 0; i < PacketWidth - 1; i++)
+                one[i] = false;
+            one[PacketWidth - 1] = true;
+
+            return Add(packet, one);
+        }
+
+        /**
+         * @brief Subtract 2 binary packets together.
+         * @param packet1 The first packet.
+         * @param packet2 The second packet, which will be subtracted from the first packet.
+         * @return The difference of the 2 packets.
+         */
+        bool *Subtract(bool packet1[PacketWidth], bool packet2[PacketWidth]) {
+            for (int i = PacketWidth - 1; i >= 0; i--) {
+                if (packet1[i] == true && packet2[i] == false) {
+                    packet1[i] = true;
+                } else if (packet1[i] == false && packet2[i] == true) {
+                    packet1[i] = true;
+                    for (int j = i - 1; j >= 0; j--) {
+                        if (packet1[j] == true) {
+                            packet1[j] = false;
+                            break;
+                        } else {
+                            packet1[j] = true;
+                        }
+                    }
+                } else {
+                    packet1[i] = false;
+                }
+            }
+
+            return packet1;
+        }
+
+        /**
+         * @brief Decrement the value of the packet by 1.
+         * @param packet The packet to decrement.
+         * @return The decremented packet.
+         */
+        bool *Decrement(bool packet[PacketWidth]) {
+            bool one[PacketWidth];
+            for (int i = 0; i < PacketWidth - 1; i++)
+                one[i] = false;
+            one[PacketWidth - 1] = true;
+
+            return Subtract(packet, one);
         }
     };
 }
